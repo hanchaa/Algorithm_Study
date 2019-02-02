368983f6469984cfcb503eac92ac0e395fb0372c
JuHan Cha <hanchaa@naver.com>
2019-02-02 08:07:45 +0000

    acmicpc.net

--- Baekjoon Online Judge/7576_토마토_x.cpp
+++ Baekjoon Online Judge/7576_토마토_x.cpp
@@ -1,72 +1,77 @@
 #include <stdio.h>
+#define MAX(a,b)(a>b?a:b)
 
-int tomato[1005][1005];
+struct koi{
+	int x;
+	int y;
+}queue[1000005];
 
-struct koi
-{
-    int x;
-    int y;
-};
 
-koi que[10000005];
-
-int main()
-{
-    freopen("input.txt","r",stdin);
-    int n,m,i,j,k,l=0,front=1,rear=1,cnt=0;
-    scanf("%d %d",&n,&m);
-    for(i=1;i<=m;i++)
-    {
-        for(j=1;j<=n;j++)
-        {
-            scanf("%d",&tomato[i][j]);
-            if(tomato[i][j]==1) { que[rear].x=i; que[rear++].y=j; }
-            if(tomato[i][j]==0) l=1;
-        }
-    }
-
-    while(front<rear && l==1)
-    {
-        k=rear;
-        for(front=front;front<k;front++)
-        {
-            if(tomato[que[front].x][que[front].y+1]==0 && que[front].y+1<=n)
-            {
-                que[rear].y=que[front].y+1;
-                que[rear++].x=que[front].x;
-                tomato[que[rear-1].x][que[rear-1].y]=1;
-            }
-            if(tomato[que[front].x+1][que[front].y]==0 && que[front].x+1<=m)
-            {
-                que[rear].x=que[front].x+1;
-                que[rear++].y=que[front].y;
-                tomato[que[rear-1].x][que[rear-1].y]=1;
-            }
-            if(tomato[que[front].x][que[front].y-1]==0 && que[front].y-1>0)
-            {
-                que[rear].y=que[front].y-1;
-                que[rear++].x=que[front].x;
-                tomato[que[rear-1].x][que[rear-1].y]=1;
-            }
-            if(tomato[que[front].x-1][que[front].y]==0 && que[front].x-1>0)
-            {
-                que[rear].x=que[front].x-1;
-                que[rear++].y=que[front].y;
-                tomato[que[rear-1].x][que[rear-1].y]=1;
-            }
-
-        }
-        cnt++;
-    }
-    cnt--;
-    for(i=1;i<=m;i++)
-    {
-        for(j=1;j<=n;j++)
-        {
-            if(tomato[i][j]==0) { cnt=-1; break; }
-        }
-        if(j<=m) break;
-    }
-    printf("%d",cnt);
-    return 0;
+int main(){
+	
+	int map[1005][1005], i, j, n = 4, m = 6, f = 1, b = 1, tmp = 0 , x, y;
+	
+	scanf("%d %d",&m ,&n);
+	
+	for(i = 0; i < n; i++){
+		for(j= 0; j < m; j++){
+			scanf("%d",&map[i][j]);
+			if(map[i][j] == 1){
+				queue[b].x = i;
+				queue[b++].y = j;
+			}
+			if(map[i][j] == 0) tmp = 1;
+		}
+	}
+	
+	if(tmp == 0) {
+		printf("0");
+		return 0;
+	}
+	
+	while(f < b){
+		
+		x = queue[f].x;
+		y = queue[f++].y;
+		
+		if(x - 1 >= 0 && map[x - 1][y] == 0){
+			map[x - 1][y] = map[x][y] + 1;
+			queue[b].x = x - 1;
+			queue[b++].y = y;
+		}
+		
+		if(y - 1 >= 0 && map[x][y - 1] == 0){
+			map[x][y - 1] = map[x][y] + 1;
+			queue[b].x = x;
+			queue[b++].y = y - 1;
+		}
+		
+		if(x + 1 < n && map[x + 1][y] == 0){
+			map[x + 1][y] = map[x][y] + 1;
+			queue[b].x = x + 1;
+			queue[b++].y = y;
+		}
+		
+		if(y + 1 < m && map[x][y + 1] == 0){
+			map[x][y + 1] = map[x][y] + 1;
+			queue[b].x = x;
+			queue[b++].y = y + 1;
+		}
+	}
+	
+	tmp = 0;
+	
+	for(i = 0; i < n; i++){
+		for(j = 0; j < m; j++){
+			if(map[i][j] == 0){
+				printf("-1");
+				return 0;
+			}
+			tmp = MAX(tmp,map[i][j]);
+		}
+	}
+	
+	printf("%d",tmp - 1);
+	
+	return 0;
 }
