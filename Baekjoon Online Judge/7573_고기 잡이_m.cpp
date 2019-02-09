4b22f046b31fa5d3a33cb9e3a9efb4ec5c45fcce
JuHan <hanchaa@gmail.com>
2019-02-09 18:00:45 +0900

    acmicpc.net

--- Baekjoon Online Judge/7573_고기 잡이_m.cpp
+++ Baekjoon Online Judge/7573_고기 잡이_m.cpp
@@ -1,39 +1,51 @@
 #include <stdio.h>
+#include <string.h>
 #include <algorithm>
-#define M 10005
 #define MAX(a,b)(a>b?a:b)
 
-int map[M][M],X[105],Y[105];
+using namespace std;
 
-int main(void)
-{
-    freopen("input.txt","r",stdin);
-    int n,l,m,i,j,k,o,max=0,I,J,cnt;
-    scanf("%d %d %d",&n,&l,&m);
-    for(i=1;i<=m;i++)
-    {
-        scanf("%d %d",&Y[i],&X[i]);
-        map[Y[i]][X[i]]=1;
-    }
-    std::sort(Y+1,Y+m+1);
-    std::sort(X+1,X+m+1);
-    for(i=1;i<=m;i++)
-    {
-        for(j=1;j<=m;j++)
-        {
-            for(k=l/2-1;k>=1;k--)
-            {
-                cnt=0;
-                for(I=Y[i];I<=Y[i]+k;I++)
-                {
-                    for(J=X[j];J<=X[j]+(l/2-k);J++)
-                    {
-                        if(map[I][J]==1) cnt++;
-                    }
-                }
-                max=MAX(max,cnt);
-            }
-        }
-    }
-    printf("%d",max);
+struct koi{
+	int x,y;
+};
+
+
+int main(){
+	
+	int l, n, m, i, j, k, cnt = 0, max = 0, x, y, dx[105], dy[105];
+	koi data[105];
+	
+	memset(dx,0,sizeof(dx));
+	memset(dy,0,sizeof(dy));
+	
+	scanf("%d %d %d", &n, &l, &m);
+	
+	for(i = 0; i < m; i++){
+		scanf("%d %d", &data[i].x, &data[i].y);
+		dx[i] = data[i].x;
+		dy[i] = data[i].y;
+	}
+	
+	sort(dx,dx+m);
+	sort(dy,dy+m);
+	
+	for(i = 0; i < m; i++){
+		for(j = 0; j < m; j++){
+			for(k = 1; k < l/2; k++){
+				
+				cnt = 0;
+				
+				for(n = 0; n < m; n++){
+					if(data[n].x <= dx[i] + k && data[n].x >= dx[i] && data[n].y <= dy[j] + l / 2 - k && data[n].y >= dy[j]) cnt++;
+				}
+				
+				max = MAX(max,cnt);
+			}
+		}
+		
+	}
+	
+	printf("%d",max);
+	
+	return 0;
 }
