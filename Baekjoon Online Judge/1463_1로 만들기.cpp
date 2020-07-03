76dafda5a4d637bf758b65fbf8ee3d56d93c835e
JuHan <hanchaa@gmail.com>
2020-07-03 20:35:49 +0900

    UCPC 5th day DP

--- Baekjoon Online Judge/1463_1로 만들기.cpp
+++ Baekjoon Online Judge/1463_1로 만들기.cpp
@@ -1,22 +1,28 @@
-#define _CRT_SECURE_NO_WARNINGS
+#pragma warning (disable : 4996)
 #include <stdio.h>
 
-#define MIN(a, b)(a < b ? a : b)
-#define N 1000005
+#define MIN(a,b)(a<b?a:b)
 
-int t[N];
+int data[1000005];
 
 int main() {
-	int n, i;
+	int n;
 
 	scanf("%d", &n);
 
-	for (i = 2; i <= n; i++) {
-		t[i] = t[i - 1] + 1;
-		if (i % 2 == 0) t[i] = MIN(t[i], t[i / 2] + 1);
-		if (i % 3 == 0) t[i] = MIN(t[i], t[i / 3] + 1);
+	for (int i = 2; i <= n; i++) {
+		data[i] = 2e9;
+
+		if (i % 3 == 0)
+			data[i] = MIN(data[i], data[i / 3] + 1);
+
+		if (i % 2 == 0)
+			data[i] = MIN(data[i], data[i / 2] + 1);
+
+		data[i] = MIN(data[i], data[i - 1] + 1);
 	}
 
-	printf("%d\n", t[n]);
+	printf("%d\n", data[n]);
+
 	return 0;
 }
--- Baekjoon Online Judge/9465_스티커.cpp
+++ Baekjoon Online Judge/9465_스티커.cpp
@@ -1,33 +1,32 @@
-#define _CRT_SECURE_NO_WARNINGS
+#pragma warning (disable : 4996)
 #include <stdio.h>
 
-#define MAX(a, b)(a > b ? a : b)
-#define N 50005
+#define MAX(a,b)(a>b?a:b)
 
-int t1[N], t2[N], map[5][N];
+int t1[100000], t2[100000], data[2][100000];
 
 int main() {
-	int t;
+	int n, t;
+
 	scanf("%d", &t);
-	for (int a = 1; a <= t; a++) {
-		int n, m, i, j, k;
 
+	for (int k = 0; k < t; k++) {
 		scanf("%d", &n);
-		for (i = 1; i <= 2; i++) {
-			for (j = 1; j <= n; j++) {
-				scanf("%d", &map[i][j]);
-			}
+
+		for (int i = 0; i < 2; i++)
+			for (int j = 0; j < n; j++)
+				scanf("%d", &data[i][j]);
+
+		t1[0] = data[0][0];
+		t2[0] = data[1][0];
+
+		for (int i = 1; i < n; i++) {
+			t1[i] = MAX(t1[i - 1], t2[i - 1] + data[0][i]);
+			t2[i] = MAX(t2[i - 1], t1[i - 1] + data[1][i]);
 		}
 
-		t1[1] = map[1][1];
-		t2[1] = map[2][1];
-
-		for (i = 2; i <= n; i++) {
-			t1[i] = MAX(t2[i - 1] + map[1][i], MAX(t1[i - 2] + map[1][i], t1[i - 1]));
-			t2[i] = MAX(t1[i - 1] + map[2][i], MAX(t2[i - 2] + map[2][i], t2[i - 1]));
-		}
-
-		printf("%d\n", MAX(t1[n], t2[n]));
+		printf("%d\n", MAX(t1[n - 1], t2[n - 1]));
 	}
+
 	return 0;
 }
