1cfa0b27bf369ca03cac691e2c35c12e2ada563b
JuHan <hanchaa@gmail.com>
2020-07-05 23:04:48 +0900

    UCPC 6th day DP

--- Baekjoon Online Judge/11055_가장 큰 증가 부분 수열.cpp
+++ Baekjoon Online Judge/11055_가장 큰 증가 부분 수열.cpp
@@ -1,33 +1,28 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#define MAX(a, b)(a > b ? a : b)
-#define N 1005
-
-int t[N], map[N];
+#define MAX(a,b)(a>b?a:b)
 
 int main() {
-	int n, m, i, j, k;
+	int dp[1000], data[1000], n, ans = 0;
 
 	scanf("%d", &n);
 
-	for (i = 1; i <= n; i++) {
-		scanf("%d", &map[i]);
-		t[i] = map[i];
+	for (int i = 0; i < n; i++) {
+		scanf("%d", data + i);
+		dp[i] = data[i];
 	}
 
-	for (i = 2; i <= n; i++) {
-		for (j = i - 1; j >= 1; j--) {
-			if (map[i] > map[j]) {
-				t[i] = MAX(t[i], t[j] + map[i]);
-			}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (data[i] > data[j])
+				dp[i] = MAX(dp[i], dp[j] + data[i]);
 		}
 	}
 
-	k = 0;
+	for (int i = 0; i < n; i++)
+		ans = MAX(ans, dp[i]);
 
-	for (i = 1; i <= n; i++) k = MAX(k, t[i]);
+	printf("%d\n", ans);
 
-	printf("%d\n", k);
 	return 0;
 }
--- Baekjoon Online Judge/16500_문자열 판별.cpp
+++ Baekjoon Online Judge/16500_문자열 판별.cpp
@@ -1,15 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 	char str[100] = { 0 }, data[100][100] = { 0 };
 	int n;
-	bool possible[100] = { 0 }, flag = true;
+	bool possible[100] = { 0 };
 
 	scanf("%s", str);
 	scanf("%d", &n);
 
+	int sLen = strlen(str);
+
 	for (int i = 0; i < n; i++)
 		scanf("%s", data[i]);
+
+	for (int i = 0; i < sLen; i++) {
+		for (int j = 0; j < n; j++) {
+			int dLen = strlen(data[j]);
+
+			if (i + dLen <= sLen) {
+				if (strncmp(str + i, data[j], dLen) == 0 && (i == 0 || possible[i - 1]))
+					possible[i + dLen - 1] = true;
+			}
+		}
+	}
+
+	printf("%d\n", possible[sLen - 1]);
 
 	return 0;
 }
