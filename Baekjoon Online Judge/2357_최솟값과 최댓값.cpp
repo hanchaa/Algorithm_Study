eb38e1851ed5a88a8002626ffadfb53615083852
hanchaa <hanchaa@gmail.com>
2020-07-22 01:05:01 +0900

    UCPC 세그먼트 트리

--- Baekjoon Online Judge/2357_최솟값과 최댓값.cpp
+++ Baekjoon Online Judge/2357_최솟값과 최댓값.cpp
@@ -6,6 +6,43 @@
 
 int minSegTree[262145], maxSegTree[262145];
 
+int minQuery(int idx, int leftBound, int rightBound, int queryL, int queryR) {
+	if (queryL <= leftBound && rightBound <= queryR)
+		return minSegTree[idx];
+
+	int mid = (leftBound + rightBound) / 2;
+
+	int lMin = INF, rMin = INF;
+
+	if (queryL <= mid && leftBound <= queryR)
+		lMin = minQuery(idx * 2, leftBound, mid, queryL, queryR);
+
+	if (queryL <= rightBound && mid + 1 <= queryR)
+		rMin = minQuery(idx * 2 + 1, mid + 1, rightBound, queryL, queryR);
+
+	return MIN(lMin, rMin);
+}
+
+int maxQuery(int idx, int leftBound, int rightBound, int queryL, int queryR) {
+	if (queryL > rightBound || queryR < leftBound)
+		return 0;
+
+	if (queryL <= leftBound && rightBound <= queryR)
+		return maxSegTree[idx];
+
+	int mid = (leftBound + rightBound) / 2;
+
+	int lMax = 0, rMax = 0;
+
+	if (queryL <= mid && leftBound <= queryR)
+		lMax = maxQuery(idx * 2, leftBound, mid, queryL, queryR);
+
+	if (queryL <= rightBound && mid + 1 <= queryR)
+		rMax = maxQuery(idx * 2 + 1, mid + 1, rightBound, queryL, queryR);
+
+	return MAX(lMax, rMax);
+}
+
 int main() {
 	int n, m, size = 1;
 
@@ -29,7 +66,19 @@ int main() {
 		maxSegTree[i] = MAX(maxSegTree[i * 2], maxSegTree[i * 2 + 1]);
 	}
 
+	// 루프부터 리프로 내려가는 쿼리
 	for (int i = 0; i < m; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+
+		printf("%d %d\n", minQuery(1, 1, size, a, b), maxQuery(1, 1, size, a, b));
+	}
+
+	return 0;
+}
+
+/* 리프부터 루프로 올라가는 쿼리
+for (int i = 0; i < m; i++) {
 		int a, b, min = INF, max = 0;
 		scanf("%d %d", &a, &b);
 
@@ -74,6 +123,4 @@ int main() {
 
 		printf("%d %d\n", min, max);
 	}
-
-	return 0;
-}
+*/
