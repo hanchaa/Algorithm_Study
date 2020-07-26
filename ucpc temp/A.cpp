fc1322ecfa26df23373210ea3e086a450d08c847
hanchaa <hanchaa@gmail.com>
2020-07-26 16:03:07 +0900

    A 추가

---
+++ ucpc temp/A.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, c, d, e, f;
+	scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+
+	for (int x = -999; x <= 999; x++) {
+		for (int y = -999; y <= 999; y++) {
+			if (a * x + b * y == c && d * x + e * y == f) {
+				printf("%d %d", x, y);
+				return 0;
+			}
+		}
+	}
+}
